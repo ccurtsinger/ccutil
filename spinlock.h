f027ef074a944066695203cc68d0fded4500d434
Charlie Curtsinger <charlie@cs.umass.edu>
2014-09-22 11:23:26 -0400

    Moved some utilities over from coz

---
+++ spinlock.h
@@ -0,0 +1,26 @@
+#if !defined(CCUTIL_SPINLOCK_H)
+#define CCUTIL_SPINLOCK_H
+
+#include <atomic>
+
+class spinlock {
+public:
+  inline void lock() {
+    while(_flag.test_and_set()) {
+      __asm__("pause");
+    }
+  }
+  
+  inline bool trylock() {
+    return !_flag.test_and_set();
+  }
+  
+  inline void unlock() {
+    _flag.clear();
+  }
+  
+private:
+  std::atomic_flag _flag = ATOMIC_FLAG_INIT;
+};
+
+#endif
